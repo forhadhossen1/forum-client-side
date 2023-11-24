33b2b2ddb68025ea8049b3b02360d3918f2d13fa
forhadhossen1 <mr.forhad1030@gamil.com>
2023-11-24 22:31:07 +0600

    setup project with vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1 className='text-5xl  text-center font-extrabold'>Vite + React</h1>
+
+    </>
+  )
+}
+
+export default App
