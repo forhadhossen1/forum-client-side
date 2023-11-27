4315820c8c091d443d89b687efbca0f47d7f5159
forhadhossen1 <mr.forhad1030@gamil.com>
2023-11-27 18:28:50 +0600

    added error page

--- src/Routers/Router.jsx
+++ src/Routers/Router.jsx
@@ -3,11 +3,13 @@ import Main from "../Layouts/Main";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Main></Main>,
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 path: '/',
