a069d06561a3b72e974256337c5740e3373e41dd
forhadhossen1 <mr.forhad1030@gamil.com>
2023-11-30 16:17:33 +0600

    added admin profile

--- src/Pages/DashboardPages/AdminPages/AdminProfile.jsx
+++ src/Pages/DashboardPages/AdminPages/AdminProfile.jsx
@@ -1,8 +1,40 @@
+import { FcBusinessman } from "react-icons/fc";
+import useAuth from "../../../Hooks/useAuth";
 
 const AdminProfile = () => {
+    const { user, loading } = useAuth();
+
+    if (loading) {
+        return <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+        }}>
+            <div className="w-24 h-24 border-4 border-dashed rounded-full animate-spin dark:border-violet-400 flex justify-center items-center"></div>
+        </div>
+
+    }
     return (
-        <div>
-            yesh admin profile
+        <div className="flex flex-col min-h-screen items-center justify-center bg-gray-200">
+            
+            <h2 className="text-5xl font-bold text-center block tracking-normal antialiased relative text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+                <span>Hi, Welcome </span>
+                {
+                    user?.displayName ? user.displayName : 'Back'
+                }
+            </h2>
+
+            <div className="flex justify-center py-5">
+                {user && user.photoURL ? (
+                    <img src={user.photoURL} alt="User" className="w-[200px] rounded-full" />
+                ) : (
+                    <FcBusinessman className="w-[200px] rounded-full text-8xl" />
+                )}
+            </div>
+            <p className="text-center text-3xl font-bold py-5">Admin Name : {user?.displayName}</p>
+            <p className="text-center text-3xl font-bold">Email : {user?.email}</p>
+
         </div>
     );
 };
--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,25 +1,29 @@
+
 import { Link } from "react-router-dom";
 
 const Banner = () => {
+     
+    const handleSearch = e => {
+        e.preventDefault();
+        const searchText = e.target.search.value;
+        console.log(searchText);
+    }
     return (
         <section>
             <div className="dark:bg-violet-400 bg-slate-100">
                 <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 dark:text-gray-900">
                     <h1 className="text-5xl font-bold leadi sm:text-6xl xl:max-w-3xl dark:text-gray-900">Welcome to Disputo Forum</h1>
                     <p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl dark:text-gray-900">The most popular forum on the internet!</p>
-                    <div className="flex items-center border border-gray-300 rounded-md overflow-hidden">
+                    <form onSubmit={handleSearch} className="flex items-center border border-gray-300 rounded-md overflow-hidden">
                         <input
                             type="text"
+                            name="search"
                             placeholder="Search..."
                             className="py-2 px-4 w-full focus:outline-none"
                         />
-                        <button
-                            type="submit"
-                            className="bg-blue-500 text-white px-4 py-2 border-l border-blue-500"
-                        >
-                            Search
-                        </button>
-                    </div>
+
+                        <input type="submit" value="Search" className="bg-blue-500 text-white px-4 py-2 border-l border-blue-500" />
+                    </form>
                 </div>
             </div>
             <div className="flex flex-col md:flex-row w-5/6 mx-auto mb-12 -mt-20 rounded-lg shadow-md lg:-mt-40 dark:bg-gray-500 bg-slate-200" >
